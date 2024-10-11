ceefb3d64f69f29fed83583df3b61c6d512689c9
Michael Yan <rain@rainboyan.com>
2024-10-11 17:13:19 +0800

    Update Examples
    Upgrade to Grace 2023.1.0-M1
    
    Closes gh-3

--- examples/pubsub-demo/app/init/pubsub/demo/Application.groovy
+++ examples/pubsub-demo/app/init/pubsub/demo/Application.groovy
@@ -1,9 +1,7 @@
 package pubsub.demo
 
 import grails.boot.Grails
-import org.springframework.context.annotation.ComponentScan
 
-@ComponentScan
 class Application {
     static void main(String[] args) {
         Grails.run(Application, args)
